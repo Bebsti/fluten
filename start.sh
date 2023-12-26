c11e05012bdf4151588461f7121d969593aaf6a4
Bebsti <129532033+Bebsti@users.noreply.github.com>
2023-12-26 22:37:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /fluten
 fi
-cd /VJ-FILTER-BOT 
+cd /fluten 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
