6b7007187b73e100e9cf6e3b689f49b57276ca67
Bebsti <129532033+Bebsti@users.noreply.github.com>
2023-12-26 22:47:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Bebsti/fluten.git
+  git clone https://github.com/Bebsti/fluten.git /fluten
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /fluten
