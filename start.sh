9cf627cc109258ea437a61e94f26dc5c653e118b
Bebsti <129532033+Bebsti@users.noreply.github.com>
2023-12-27 11:36:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,16 @@
-if [ -z $UPSTREAM_REPO ]
-then
+#!/bin/bash
+
+if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
   git clone https://github.com/Bebsti/fluten.git /VJ-FILTER-BOT 
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone "$UPSTREAM_REPO" /VJ-FILTER-BOT 
 fi
+
 cd /VJ-FILTER-BOT 
+
 pip3 install -U -r requirements.txt
+
 echo "Starting Bot...."
 python3 bot.py
